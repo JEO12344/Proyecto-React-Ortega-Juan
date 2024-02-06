887ff758ef6a014e0303a96712948ed564a12244
Juan Emilio <ortegarisso@gmail.com>
2024-02-06 15:17:48 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import Orgullo from "./imagenes/orgullo.jpg";
 import Alicia from "./imagenes/alicia.png";
 import Frankestein from "./imagenes/frankenstein.jpg";
 
-
 function App() {
     return (
         <div>
+            <Navbar />
             <h1>Libros</h1>
             <ItemListContainer titulo1="Orgullo y Prejuicio" precio="5000" img={Orgullo} />
             <ItemListContainer titulo2="Alicia en el Pais de las maravilla" precio="5700" img={Alicia} />
             <ItemListContainer titulo3="Frankestein" precio="6000" img={Frankestein} />
         </div>
-)
+    );
 }
+
 export default App;
